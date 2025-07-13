ca2515cfdd1162f1d3d04f027e2f8a745f4d2f97
Jithin KS <jithinks@Jithins-MacBook-Pro-2.local>
2025-07-13 16:46:02 +0530

    smart contract baby steps

--- src/ContentStorage.sol
+++ src/ContentStorage.sol
@@ -3,12 +3,27 @@ pragma solidity ^0.8.13;
 
 contract ContentStorage {
     mapping(address => string[]) private contentsOfUser;
+    mapping(address => mapping(bytes32 => bool)) private userContentHashes;
 
-    function addContent(address user, string memory content) public {
-        contentsOfUser[user].push(content);
+    function addContent(string memory content) public {
+        bytes32 contentHash = keccak256(abi.encodePacked(content));
+        
+        if (doesContentExist(msg.sender, content)) {
+            revert contentExists(msg.sender, content);
+        }
+        
+        contentsOfUser[msg.sender].push(content);
+        userContentHashes[msg.sender][contentHash] = true;
     }
 
-    function getContentsOfUser(address user) public view returns (string[] memory) {
-        return contentsOfUser[user];
+    function getContentsOfUser() public view returns (string[] memory) {
+        return contentsOfUser[msg.sender];
     }
+
+    function doesContentExist(address user, string memory content) public view returns (bool) {
+        bytes32 contentHash = keccak256(abi.encodePacked(content));
+        return userContentHashes[user][contentHash];
+    }
+
+    error contentExists(address user, string content);
 }
--- test/ContentStorage.t.sol
+++ test/ContentStorage.t.sol
@@ -12,13 +12,13 @@ contract ContentStorageTest is Test {
     }
 
     function test_AddAndGetContentsOfUser() public {
-        contentStorage.addContent(address(this), "content1");
-        contentStorage.addContent(address(this), "content2");
-        contentStorage.addContent(address(this), "content3");
+        contentStorage.addContent("content1");
+        contentStorage.addContent("content2");
+        contentStorage.addContent("content3");
         
         // assert content 1, 2, 3 are in the contents of the user
-        assertEq(contentStorage.getContentsOfUser(address(this))[0], "content1");
-        assertEq(contentStorage.getContentsOfUser(address(this))[1], "content2");
-        assertEq(contentStorage.getContentsOfUser(address(this))[2], "content3");
+        assertEq(contentStorage.getContentsOfUser()[0], "content1");
+        assertEq(contentStorage.getContentsOfUser()[1], "content2");
+        assertEq(contentStorage.getContentsOfUser()[2], "content3");
     }
 }
